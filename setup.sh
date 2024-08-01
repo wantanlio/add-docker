9d85483dca2cc46ed5fddd5cbc6104b776887475
wantanlio <156288450+wantanlio@users.noreply.github.com>
2024-08-01 12:18:19 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,11 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo touch /etc/docker/daemon.json
+sudo cat <<EOF > /etc/docker/daemon.json
+{
+	"log-driver": "local",
+	"log-opts": {"max-size": "10m", "max-file": "3"}
+}
+EOF
+systemctl restart docker
